8b5aaa37eb70e034c6fe47da5af2a5f1e1464d60
JelmerPastoor <jelmer.pastoor@gmail.com>
2024-06-27 23:59:16 +0200

    RPS finished poggies

--- rps.py
+++ rps.py
@@ -1,64 +1,27 @@
 import numpy as np
-print('Welcome to Rock Paper Scissors!')
-user_play = input('Please enter Rock, Paper, or Scissors!\n')
+pc_choices = ['Rock', 'Paper', 'Scissors']
 
-# Rules: Rock beats scissors, scissors beats paper, paper beats rock
-def computer():
-    pc_choices = ['Rock', 'Paper', 'Scissors']
+def game():
+    print('Welcome to Rock Paper Scissors!')
+    user_play = input('Please enter Rock, Paper, or Scissors!\n')
     pc_play = np.random.choice(pc_choices)
-    return pc_play
+    if pc_play.lower() == user_play.lower():
+        print('The game is a draw, please play again!')
+    elif ((user_play.lower() == 'rock' and pc_play.lower() == 'paper') or 
+          (user_play.lower() == 'paper' and pc_play.lower() == 'scissors') or 
+          (user_play.lower() == 'scissors' and pc_play.lower() == 'rock')):
+        print('You have unfortunately lost, please play again!')
+    if ((user_play.lower() == 'rock' and pc_play.lower() == 'scissors') or 
+          (user_play.lower() == 'paper' and pc_play.lower() == 'rock') or 
+          (user_play.lower() == 'scissors' and pc_play.lower() == 'paper')):
+        print('You have won, congratulations!')
+    return
 
-if user_play.lower() == 'rock' or user_play == 'Rock':
-    if computer() == 'Rock':
-        print('The game is a draw, please play again!')
-        # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-    if computer() == 'Scissors':
-        result = input('You have won, do you want to play again? [Y/N]\n')
-        if result == 'Y':
-            print('Play again')
-            # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-        if result == 'N':
+def main():
+    while True:
+        game()
+        play_again = input('Play again? [Y/N]: ') == 'Y'
+        if not play_again:
             print('Thanks for playing, have a good day!')
-    if computer() == 'Paper':
-        result = input('You have unfortunately lost, do you want to play again? [Y/N]\n')
-        if result == 'Y':
-            print('Play again')
-            # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-        if result == 'N':
-            print('Thanks for playing, have a good day!')
-if user_play.lower() == 'sciccors' or user_play == 'Scissors':
-    if computer() == 'Scissors':
-        print('The game is a draw, please play again!')
-        # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-    if computer() == 'Paper':
-        result = input('You have won, do you want to play again? [Y/N]\n')
-        if result == 'Y':
-            print('Play again')
-            # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-        if result == 'N':
-            print('Thanks for playing, have a good day!')
-    if computer() == 'Rock':
-        result = input('You have unfortunately lost, do you want to play again? [Y/N]\n')
-        if result == 'Y':
-            print('Play again')
-            # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-        if result == 'N':
-            print('Thanks for playing, have a good day!')
-if user_play.lower() == 'paper' or user_play == 'Paper':
-    if computer() == 'Paper':
-        print('The game is a draw, please play again!')
-        # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-    if computer() == 'Rock':
-        result = input('You have won, do you want to play again? [Y/N]\n')
-        if result == 'Y':
-            print('Play again')
-            # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-        if result == 'N':
-            print('Thanks for playing, have a good day!')
-    if computer() == 'Scissors':
-        result = input('You have unfortunately lost, do you want to play again? [Y/N]\n')
-        if result == 'Y':
-            print('Play again')
-            # Nu nog hier maken dat je dan opnieuw begint, moet met een def dan kan je hem opnieuw aanroepen
-        if result == 'N':
-            print('Thanks for playing, have a good day!')
+            return
+main()
